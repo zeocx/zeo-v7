33ceef600813abc194266f1d153c35a4592c1072
zeocx <gilangabied@gmail.com>
2022-01-17 20:16:16 +0700

    zeo-v7

---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0887 4350 47326
+2. BCA 
+	ㅁ 3901 0887 4350 47326
+3. BNI 
+	ㅁ 881 0887 4350 47326
+4. PERMATA
+	ㅁ 852 8887 4350 47326
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0895 1620 7932
+2. OVO
+	ㅁ 0895 1620 7932
+3. DANA
+	ㅁ 0895 1620 7932
+	
+Semuanya atas nama ZEO
+`
+	}
+
+exports.donasibot = donasibot
+ 
